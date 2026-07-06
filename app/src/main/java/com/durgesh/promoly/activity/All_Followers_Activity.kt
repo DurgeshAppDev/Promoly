798b2e7e13c776de855eeb5b9294b4a793ad7f68
Durgesh Kumar <mr.durgeshkumar55@gmail.com>
2026-07-06 17:46:07 +0530

    .

---
+++ app/src/main/java/com/durgesh/promoly/activity/All_Followers_Activity.kt
@@ -0,0 +1,21 @@
+package com.durgesh.promoly.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.durgesh.promoly.R
+
+class All_Followers_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_all_followers)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
